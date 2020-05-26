33f7899fd89a1d314888d44b0cee64a4828cfe51
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-05-26 16:00:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+   "Hello, my name is #{name}."
+  
+end 
+
+def batch_badge_creator(name)
+  name.map do |names|
+     "Hello, my name is #{names}."
+  
+  end 
+end 
+
+def assign_rooms (person)
+  
+  
+end 
